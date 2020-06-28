6f23173913b5215b18756629ab4df14dfe600075
fox0 <red-fox0@yandex.ru>
2020-06-28 18:02:29 +0700

    *DoH

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
-use std::io;
-use rand::Rng;
+// use std::net::*;
+use trust_dns_resolver::Resolver;
+use trust_dns_resolver::config::{ResolverConfig, ResolverOpts};
+
 
 fn main() {
-    println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_number);
+    let resolver = Resolver::new(ResolverConfig::cloudflare_tls(),
+                                     ResolverOpts::default()).unwrap();
 
-    println!("Please input your guess.");
-    let mut line = String::new();
-    io::stdin().read_line(&mut line).unwrap();
-
-    let number = line.trim().parse().unwrap();
-    if secret_number < number {
-        println!("<");
-    } else if secret_number > number {
-        println!(">");
-    } else {
-        println!("==");
+    let response = resolver.lookup_ip("derpibooru.org").unwrap();
+    let ip;
+    for i in response.iter() {
+        ip = i;
+        break;
     }
+    dbg!(ip);
+
+    // There can be many addresses associated with the name,
+    //  this can return IPv4 and/or IPv6 addresses
+    // let address = response.next().expect("no addresses returned!");
+    // if address.is_ipv4() {
+    //     assert_eq!(address, IpAddr::V4(Ipv4Addr::new(93, 184, 216, 34)));
+    // } else {
+    //     assert_eq!(address, IpAddr::V6(Ipv6Addr::new(0x2606, 0x2800, 0x220, 0x1, 0x248, 0x1893, 0x25c8, 0x1946)));
+    // }
 }
